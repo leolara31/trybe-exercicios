b34eba4cc498f0d239583f6165fbd05c2b6c47c9
Leonardo G <leonardog.delara@gmail.com>
2022-06-21 23:05:45 -0300

    exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -71,3 +71,24 @@ if (pecaDeXadrez.toLowerCase() === "bispo") {
 } else {
   console.log("erro");
 }
+
+//exercicio 7
+
+const nota = 150;
+if(nota <= 0 || nota >100) {
+    console.log('erro')
+}else if(nota >= 90) {
+    console.log('A')
+}else if (nota >= 80) {
+    console.log('B')
+}else if (nota >= 70) {
+    console.log('C')
+}else if (nota >= 60) {
+    console.log('D')
+}else if (nota >= 50) {
+    console.log('E')
+}else if (nota >=- 49) {
+    console.log('F')
+}else if (nota <= 0 || nota >100) {
+    console.log('erro')
+}
