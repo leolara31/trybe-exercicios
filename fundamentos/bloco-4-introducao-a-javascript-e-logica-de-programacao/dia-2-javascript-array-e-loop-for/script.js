4508a51732145c1b4e2555a6bc7ce3ae9de47f0d
Leonardo G <leonardog.delara@gmail.com>
2022-06-22 19:10:22 -0300

    2 exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,5 +1,15 @@
+//exercicio 1 (os exercicio do 1 ao 7 iram utilizar o let number declarado no inicio)
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
+
+//exercicio 2
+
+let soma = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+console.log(soma);
