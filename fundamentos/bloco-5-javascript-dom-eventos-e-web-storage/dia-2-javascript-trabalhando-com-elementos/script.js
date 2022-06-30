75fba7f1995b65497122456692d99e104fd83c38
Leonardo G <leonardog.delara@gmail.com>
2022-06-29 21:35:55 -0300

    exercicio 2 e 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,3 +1,11 @@
-let titulo = document.createElement('h1');
+let titulo = document.createElement("h1");
 document.body.appendChild(titulo);
-titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
+titulo.innerText = "Exercício 5.2 - JavaScript DOM";
+
+let novaMain = document.createElement("main");
+novaMain.className = "main-content";
+document.body.appendChild(novaMain);
+
+let secao = document.createElement("section");
+secao.className = "center-content";
+novaMain.appendChild(secao);
